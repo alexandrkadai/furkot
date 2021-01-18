60b65900a0a386d851f408411d6a42a120016e30
kaldikonly <kaldikonly@gmail.com>
2021-01-18 21:30:27 +0200

    first change

---
+++ asset/main.js
@@ -0,0 +1,12 @@
+
+const showMenu = () =>{
+ let x = document.getElementById("navig");
+ if(x.style.left === "-100%"){
+     x.style.left = "0"
+ }
+ else {
+     x.style.left = "-100%"
+ }
+}
+
+document.getElementById('icon-menu').addEventListener('click', showMenu);
