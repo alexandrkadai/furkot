5b565d99b2d375004001a91dc98344db8e06f13d
alex kadai <alexandrkadai@gmail.com>
2022-10-11 23:48:45 +0300

    Converting to scss

--- asset/main.js
+++ asset/main.js
@@ -1,12 +1,10 @@
+const showMenu = () => {
+  let x = document.getElementById('navig');
+  if (x.style.left === '-100%') {
+    x.style.left = '0';
+  } else {
+    x.style.left = '-100%';
+  }
+};
 
-const showMenu = () =>{
- let x = document.getElementById("navig");
- if(x.style.left === "-100%"){
-     x.style.left = "0"
- }
- else {
-     x.style.left = "-100%"
- }
-}
-
-document.getElementById('icon-menu').addEventListener('click', showMenu);
+document.getElementById('icon-menu__container').addEventListener('click', showMenu);
