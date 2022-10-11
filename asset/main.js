2a85abc96604db3949f213b061e40a1138dfa045
alex kadai <alexandrkadai@gmail.com>
2022-10-11 23:54:47 +0300

    Making js

--- asset/main.js
+++ asset/main.js
@@ -1,7 +1,12 @@
 const showMenu = () => {
   let x = document.getElementById('navig');
+  let y = document.getElementById('icon-menu');
+  let yAfter = window.getComputedStyle(y, "::after");
+  let yBefore = window.getComputedStyle(y, "::before");
   if (x.style.left === '-100%') {
     x.style.left = '0';
+    y.style.display = 'none';
+    yAfter.style.translate = 
   } else {
     x.style.left = '-100%';
   }
